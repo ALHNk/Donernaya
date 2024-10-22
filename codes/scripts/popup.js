f2654cec3106c1b69cd3d0fe8d70f86be55722d4
ALHNk <88340494+ALHNk@users.noreply.github.com>
2024-10-22 17:37:54 +0500

    Callback

--- codes/scripts/popup.js
+++ codes/scripts/popup.js
@@ -1,4 +1,27 @@
+let checkContainer = document.getElementById('after-form-check');
+checkContainer.style.display = 'none';
+
 function togglePopup() {
     const overlay = document.getElementById('popupOverlay');
     overlay.classList.toggle('show');
 }
+
+function greet(email, callback){
+    document.getElementById('pop-up-name').innerText = `Welcome!, ${email}`;
+    callback();
+}
+function greetAndCheck(){
+    document.getElementById('form-con').style.display = 'none';
+    checkContainer.style.display = 'block';
+    document.getElementById('pop-up-name').innerText = `is it you?`;
+}
+
+function popUpSubmit(){
+    
+    let emailInput = document.getElementsByClassName('form-input')[0].value;
+    if(emailInput != null)
+    {        
+        greet(emailInput, greetAndCheck);
+    }
+}
+
